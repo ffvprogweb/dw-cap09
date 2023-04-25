0dca619f2bf25c57e49f1ee43210e9b73f792e17
ffvprogweb <disclabes@gmail.com>
2023-04-24 21:53:32 -0300

    Refatoração dos imports inclusao do arquivo system.properties

--- sig1/src/main/java/com/fatec/sig1/controller/GUIPedidoController.java
+++ sig1/src/main/java/com/fatec/sig1/controller/GUIPedidoController.java
@@ -1,13 +1,12 @@
 package com.fatec.sig1.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-import javax.validation.Valid;
+
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
--- sig1/src/main/java/com/fatec/sig1/model/PedidoRepository.java
+++ sig1/src/main/java/com/fatec/sig1/model/PedidoRepository.java
@@ -4,7 +4,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import com.fatec.sig1.model.Pedido;
+
 
 @Repository
 public interface PedidoRepository extends JpaRepository <Pedido, Long> {
